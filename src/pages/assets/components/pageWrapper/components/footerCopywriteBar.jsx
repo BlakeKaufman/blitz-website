24d98692cb8dad316f988aebc7b4b9b53e958f70
Blake Kaufman <blakekaufman9@gmail.com>
2024-05-20 13:27:35 -0400

    bottom footer bar finished

--- src/pages/assets/components/pageWrapper/components/footerCopywriteBar.jsx
+++ src/pages/assets/components/pageWrapper/components/footerCopywriteBar.jsx
@@ -6,10 +6,20 @@ export default function FooterCopywriteBar() {
     <div className="copywriteBar">
       <p>Blitz Wallet © 2023</p>
       <div className="socials">
-        <div className="socialIcon">
-          <img src={ICONS.githubIcon} alt="" srcset="" />
-        </div>
-        <div className="socialIcon"></div>
+        <a
+          target="_blank"
+          href="https://github.com/BlakeKaufman/BlitzWallet"
+          className="socialIcon"
+        >
+          <img src={ICONS.githubIcon} alt="github icon to link to code" />
+        </a>
+        <a
+          target="_blank"
+          href="https://x.com/BlitzWallet1"
+          className="socialIcon"
+        >
+          <img src={ICONS.twitterIcon} alt="github icon to link to code" />
+        </a>
       </div>
     </div>
   );
